3d4cc5e642319221e5ce17feaadb2a4ee0283192
Ranjeet0605 <kranjeet0829@gmail.com>
2023-02-09 22:00:22 +0530

    massage add

--- src/pages/ProductPage/index.js
+++ src/pages/ProductPage/index.js
@@ -1,33 +1,35 @@
 
-import "./index.css"
-import CardContainer from '../../components/CardContainer'
-const Component = () =>{
-    return (<>
-                <div className="productPage">
-                    <div className="productImg">
-                        <img src="https://cdn.shopclues.com/images1/thumbnails/112930/320/320/151885535-112930695-1653064733.jpg" 
-                        style={{width:"100%",height:"100%"}} alt="not availabe"/>
-                    </div>
-                    <div className="productDetails">
-                        <h4>Boat Headset..</h4>
-                        <div className="itemSold">
-                            Item sold : 44
-                        </div>
-                        <div style={{display:"flex",alignItems:'flex-end',gap:'4px'}}>
-                            <div style={{fontSize:"26px",fontWeight:"500"}}>
-                                ₹599 
-                            </div>                            
-                            <div style={{fontWeight:"500",color:"gray",fontSize:"16px",marginBottom:"6px"}}>
-                                <strike>
-                                    ₹900 
-                                </strike>
-                                &nbsp;
-                                <span style={{color:"green"}}>
-                                    20% Off
-                                </span>
-                            </div>
-                        </div>
-
+// import "./index.css"
+// import CardContainer from '../../components/CardContainer'
+// const Component = () =>{
+//     return (
+//     <>
+//                 <div className="productPage">
+//                     <div className="productImg">
+//                         <img src="https://cdn.shopclues.com/images1/thumbnails/112930/320/320/151885535-112930695-1653064733.jpg" 
+//                         style={{width:"100%",height:"100%"}} alt="not availabe"/>
+//                     </div>
+//                     <div className="productDetails">
+//                         <h4>Boat Headset..</h4>
+//                         <div className="itemSold">
+//                             Item sold : 44
+//                         </div>
+//                         <div style={{display:"flex",alignItems:'flex-end',gap:'4px'}}>
+//                             <div style={{fontSize:"26px",fontWeight:"500"}}>
+//                                 ₹599 
+//                             </div>                            
+//                             <div style={{fontWeight:"500",color:"gray",fontSize:"16px",marginBottom:"6px"}}>
+//                                 <strike>
+//                                     ₹900 
+//                                 </strike>
+//                                 &nbsp;
+//                                 <span style={{color:"green"}}>
+//                                     20% Off
+//                                 </span>
+//                             </div>
+//                         </div>
+// </>
+// );
 import "./index.css";
 import { useLocation } from "react-router-dom";
 import CardContainer from "../../components/CardContainer";
