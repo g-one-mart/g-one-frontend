a52ce0495b992f275fe5fef05c301458cc089945
Rajpratim Patra <rajpratim.001@gmail.com>
2023-03-04 22:46:31 +0530

    added google login, stored jwt token

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,35 +1,37 @@
 import { useState } from "react";
+import { useGoogleLogin } from "@react-oauth/google";
 import { useAuthContext } from "./useAuthContext";
+import { signInGoogle } from "../api/index";
 
 export const useLogin = () => {
   const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
-  const { dispatch } = useAuthContext;
+  const [isLoading, setIsLoading] = useState(false);
+  const { dispatch } = useAuthContext();
 
-  const login = async () => {
+  async function handleGoogleLoginSuccess(tokenResponse) {
     setIsLoading(true);
     setError(null);
-    const response = await fetch("localhost:4000/auth/google/callback", {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Credentials": true,
-      },
-    });
-    const json = await response.json;
+    const accessToken = tokenResponse.access_token;
 
-    if (!response.ok) {
-      setIsLoading(false);
-      setError(json.error);
-    }
-    if (response.ok) {
-      localStorage.setItem("user", JSON.stringify(json));
+    try {
+      const response = await signInGoogle(accessToken);
 
-      dispatch({ type: "LOGIN", payload: json });
-      setIsLoading(false);
+      if (response.statusText === "OK") {
+        const { data } = response;
+        localStorage.setItem("user", JSON.stringify(data));
+
+        dispatch({ type: "LOGIN", payload: data });
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+        setError(response.error);
+      }
+    } catch (err) {
+      console.log(err);
     }
-  };
+  }
+
+  const login = useGoogleLogin({ onSuccess: handleGoogleLoginSuccess });
+
   return { login, isLoading, error };
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import { AuthContextProvider } from "./context/authContext";
 
@@ -7,7 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <GoogleOAuthProvider clientId="964795636967-ho85cm3ndost06uqq85iehvkhvqrl3j3.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
