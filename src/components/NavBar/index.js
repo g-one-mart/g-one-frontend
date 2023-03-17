bd66224773c458c0c8626e00db850d40a1db482a
ChhutiMistry <chhuti246@gmail.com>
2023-03-17 19:07:49 +0530

    made navbar responsive

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -10,43 +10,63 @@ const Component = () => {
   const { user } = useAuthContext();
   return (
     <>
-      <div className="navBar">
-        <div>
-          <Link to={"/"}>
-            <img src="logo.png" alt="logo" width="50px" height="50px" />
-          </Link>
-        </div>
-        <div className="searchBox">
-          <div>
-            <input
-              type="text"
-              className="searchInput"
-              placeholder="Search products, brands..."
-            />
-          </div>
-          <div>
-            <button className="searchButton">
-              <i className="fa fa-search" aria-hidden="true"></i>
-            </button>
-          </div>
-        </div>
-        {user ? (
-          <div>
-            <Link to={"/user/profile"}>
-              <i className="fa fa-user fa-2x" aria-hidden="true"></i>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary navBar">
+        <div className="container-fluid">
+          <div className="navbar-brand">
+            <Link to={"/"}>
+              <img src="logo.png" alt="logo" width="50px" height="50px" />
             </Link>
-            <button onClick={() => logout()}>Logout</button>
           </div>
-        ) : (
-          <div>
-            <button disabled={isloading} onClick={() => login()}>
-              Login
-            </button>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse collnav" id="navbarScroll">
+            <ul className="navbar-nav navbar-nav-scroll">
+              <li className="nav-item">
+                <div className="searchBox">
+                  <div className="searchBoxlength">
+                    <input
+                      type="text"
+                      className="searchInput"
+                      placeholder="Search products, brands..."
+                    />
+                  </div>
+                  <div>
+                    <button className="searchButton">
+                      <i className="fa fa-search" aria-hidden="true"></i>
+                    </button>
+                  </div>
+                </div>
+              </li>
+            </ul>
+            <div className="usericonpos">
+              {user ? (
+                <div className="useicons">
+                  <Link to={"/user/profile"}>
+                    <i className="fa fa-user fa-2x usericon" aria-hidden="true"></i>
+                  </Link>
+                  <button onClick={() => logout()} className="btn usericonbutton">Logout</button>
+                </div>
+              ) : (
+                <div className="usericonof">
+                  <button disabled={isloading} onClick={() => login()} className="btn usericonbutton">
+                    Login
+                  </button>
+                </div>
+              )}
+            </div>
+            <div className="nav-item carticon">
+              <i className="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>
+            </div>
           </div>
-        )}
-      </div>
+        </div>
+      </nav>
     </>
   );
 };
 
 export default Component;
+
+
+
+      
